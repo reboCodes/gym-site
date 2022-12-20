f298009577691bf2fa1c526f5d699922fdf5bf3d
reboCodes <reggie@scerbo.dev>
2022-12-20 02:33:16 -0500

    exercises now delete on Set updates

--- src/features/workout/workoutSlice.jsx
+++ src/features/workout/workoutSlice.jsx
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// object definitions
 
 const emptySet = {
     "reps" : "",
@@ -18,23 +19,45 @@ const emptyWorkout = {
     "workout_name" : "",
     "workout_date" : "",
     "workout_notes" : "",
-    "exercies" : [{
-        "exercise_name" : "",
-        "exercise_notes" : "",
-        "sets" : [{
-            "reps" : "",
-            "weight" : "",
-            "set_difficulty" : "",
-            "set_notes" : ""
-        }]
-    }]
+    "exercies" : [ emptyExercise ]
 }
 
-const initialState = emptyWorkout;
+
+// helper functions
+
+const deleteEmptyExercises = (workoutState) => {
+    var workout = workoutState;
+    const numExercise = workout.exercies.length;
+    for(var repeat = 2; repeat>0; repeat--){
+        for(var i=0; i<=numExercise; i++){
+            const prevExercise = JSON.stringify({...workout.exercies[i-1]});
+            const currExercise = JSON.stringify({...workout.exercies[i]});
+            const empty = JSON.stringify(emptyExercise)
+            if(prevExercise === empty && currExercise === empty){
+                workout.exercies.splice(i, 1)
+            }}}
+    return workout;
+}
+
+const deleteEmptySets = (workoutState, exerciseId) => {
+    var workout = workoutState;
+    const numSets = workout.exercies[exerciseId].sets.length;
+    for(var repeat = 2; repeat>0; repeat--){
+        for(var i=0; i<=numSets; i++){
+            const prevSet = JSON.stringify({...workout.exercies[exerciseId].sets[i-1]});
+            const currSet = JSON.stringify({...workout.exercies[exerciseId].sets[i]});
+            const empty = JSON.stringify(emptySet)
+            if(prevSet === empty && currSet === empty){
+                workout.exercies[exerciseId].sets.splice(i, 1)
+            }}}
+}
+
+
+// slice definition 
 
 const workoutSlice = createSlice({
     name: "workout",
-    initialState,
+    initialState: emptyWorkout,
     reducers: {
 
         updateWorkout: (workout, action) => {
@@ -45,71 +68,35 @@ const workoutSlice = createSlice({
         updateExercise: (workout, action) => {
             const { param, value, exerciseId } = action.payload;
             workout.exercies[exerciseId][param] = value;
-
-            const numExercise = workout.exercies.length;
-            
-            if(exerciseId === numExercise-1){
+            if(exerciseId === workout.exercies.length-1){
                 workout.exercies.push(emptyExercise);
             }
-            
-            for(var repeat = 2; repeat>0; repeat--){
-                if(numExercise > 1){
-                    for(var i=0; i<=numExercise; i++){
-                        const prevExercise = JSON.stringify({...workout.exercies[i-1]});
-                        const currExercise = JSON.stringify({...workout.exercies[i]});
-                        const empty = JSON.stringify(emptyExercise)
-                        if(prevExercise === empty && currExercise === empty){
-                            workout.exercies.splice(i, 1)
-                        }
-                    }
-                }
-            }
+            if(!value){ workout = deleteEmptyExercises(workout); }
         },
 
         updateSet: (workout, action) => {
             const { param, value, exerciseId, setId } = action.payload;
             workout.exercies[exerciseId].sets[setId][param] = value;
-
-            const numSets = workout.exercies[exerciseId].sets.length;
-            
-            if(setId === numSets-1){
+            if(setId === workout.exercies[exerciseId].sets.length-1){
                 workout.exercies[exerciseId].sets.push(emptySet);
             }
-            
-            for(var repeat = 2; repeat>0; repeat--){
-                if(numSets > 1){
-                    for(var i=0; i<=numSets; i++){
-                        const prevSet = JSON.stringify({...workout.exercies[exerciseId].sets[i-1]});
-                        const currSet = JSON.stringify({...workout.exercies[exerciseId].sets[i]});
-                        const empty = JSON.stringify(emptySet)
-                        if(prevSet === empty && currSet === empty){
-                            workout.exercies[exerciseId].sets.splice(i, 1)
-                        }
-                    }
-                }
+            if(!value){
+                workout = deleteEmptyExercises(workout);
+                workout = deleteEmptySets(workout, exerciseId);
             }
-        },
-
-        getEmptySet: () => {
-            return emptySet;
-        },
-
-        getEmptyExercise: () => {
-            return emptyExercise;
         },
         
     }
 });
 
 
+// export actions and reducers
+
 export const {
     updateWorkout,
     updateExercise,
-    updateSet,
-    getEmptySet,
-    getEmptyExercise
+    updateSet
 } = workoutSlice.actions;
 
 export default workoutSlice.reducer;
-
 
